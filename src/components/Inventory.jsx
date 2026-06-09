f1c270a08120dbdd98357a63ca73c80087a8ee03
unknown <kacper2116124@gmail.com>
2026-06-09 17:29:41 +0200

    Zmieniono indeksowanie eq w kodzie na 0 based. Dodano obsluge drugiej strony eq.

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -20,49 +20,71 @@ const Inventory = () => {
             },
             img: "https://pl-wiki.metin2.gameforge.com/images/7/7a/Miecz.png",
             size: 2,
-        }
+        },
+        {
+            id: 1,
+            name: "Miecz+9",
+            from_level: 1,
+            attack_value: "13-15",
+            magical_attack_value: "15-19",
+            attack_speed: "+22%",
+            wearable: true,
+            for: ["Wojownik", "Ninja", "Sura"],
+            upgrade: {
+                yang: null,
+                upgrade_items: null,
+                upgrade_change: null,
+            },
+            img: "https://pl-wiki.metin2.gameforge.com/images/7/7a/Miecz.png",
+            size: 2,
+        },
     ]
 
+    const [currentPage, setCurrentPage] = useState(0)
+    const [X_SIZE, Y_SIZE] = [5, 9] //Wymiary inventory
 
-
-    const [X_SIZE, Y_SIZE] = [5, 9]
     const [items, setItems] = useState([
-        {
-            position: 1,
-            item: items_arr[0],
-        }
+        [
+            { item: items_arr[0], slot: 0 },
+            { item: items_arr[0], slot: 14 },
+        ],
+        [
+            { item: items_arr[0], slot: 1 }
+        ]
     ]);
 
     useEffect(() => {
         console.log(items)
     }, [items])
 
-    const isSlotEmpty = (slot) => {
-        return !items.some(({ position, item }) => {
+    const isSlotEmpty = (page, slot) => {
+
+        return !items[page].some(({ item, slot: itemSlot }) => {
             for (let i = 0; i < item.size; i++) {
-                const currentSlot = position + i * X_SIZE;
+                const currentSlot = itemSlot + i * X_SIZE;
                 if (currentSlot === slot) return true;
             }
             return false;
         })
     }
 
-    const canPlaceItem = (slot, item) => {
+    const canPlaceItem = (page, slot, item) => {
 
         for (let i = 0; i < item.size; i++) {
             const currentSlot = slot + i * X_SIZE;
 
-            if (currentSlot > X_SIZE * Y_SIZE) return false;
-            if (!isSlotEmpty(currentSlot)) return false;
+            if (currentSlot >= X_SIZE * Y_SIZE) return false;
+            if (!isSlotEmpty(page, currentSlot)) return false;
         }
+
         return true;
     }
 
-    const addItem = (item) => {
+    const addItem = (item, page) => {
         setItems((prev) => {
-            const copy = [...items]
-            copy.push(item);
-            return copy;
+            const updated = structuredClone(prev)
+            updated[page] = [...updated[page], item];
+            return updated;
         })
     }
 
@@ -70,29 +92,32 @@ const Inventory = () => {
 
         const itemToSpawn = items_arr.find(item => item.id === id);
 
-        for (let i = 1; i <= X_SIZE * Y_SIZE; i++) {
-            if (canPlaceItem(i, itemToSpawn)) {
-                addItem({ item: itemToSpawn, position: i });
-                console.log("Dodano item");
-                return;
+        for (let page = 0; page < items.length; page++) {
+
+            for (let slot = 0; slot < X_SIZE * Y_SIZE; slot++) {
+                if (canPlaceItem(page, slot, itemToSpawn)) {
+                    addItem({ item: itemToSpawn, slot: slot }, page);
+                    console.log("Dodano item");
+                    return;
+                }
             }
         }
         console.log("Nie można dodać itemu")
     }
 
-
     return (
         <div className="inventory">
             <div className="pages">
-                <div className="page page-active">I</div>
-                <div className="page">II</div>
+                <button className={`page ${currentPage === 0 && 'page-active'}`} onClick={() => setCurrentPage(0)}>I</button>
+                <button className={`page ${currentPage === 1 && 'page-active'}`} onClick={() => setCurrentPage(1)}>II</button>
             </div>
             <div className="slots">
                 {Array.from({ length: X_SIZE * Y_SIZE }).map((_, index) => {
 
-                    const item = items.find(item => item.position === index + 1);
+                    const item = items[currentPage].find(item => item.slot === index);
+
                     return (
-                        <div key={index + 1} id={`slot-${index + 1}`} className="slot">
+                        <div key={index} id={`slot-${index}`} className="slot">
                             {item &&
                                 <img className="item-img" src={item.item.img}></img>
                             }
