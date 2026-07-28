91faef463ccf2e06c7f688bdec883db6b2d96e1d
unknown <kacper2116124@gmail.com>
2026-07-28 16:15:29 +0200

    Dodano podpis z nickiem nad kowalem

--- src/components/Blacksmith.jsx
+++ src/components/Blacksmith.jsx
@@ -4,6 +4,7 @@ import blacksmithImg from '../assets/blacksmith.png'
 const Blacksmith = () => {
     return (
         <div className='blacksmith'>
+            <span className='blacksmith-nickname'>Kowal</span>
             <img className='blacksmith-img' src={blacksmithImg}></img>
         </div>
     )
