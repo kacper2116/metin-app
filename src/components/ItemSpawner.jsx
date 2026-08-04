7c153b9a1d2edb20876a6af77620ceb2b5bbeebd
unknown <kacper2116124@gmail.com>
2026-08-04 23:33:44 +0200

    Dodano plik z ulepszaczami i ich img

--- src/components/ItemSpawner.jsx
+++ src/components/ItemSpawner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/ItemSpawner.css'
 import ItemPicker from './ItemPicker';
 import ItemFilter from './ItemFilter';
