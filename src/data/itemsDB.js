6d4ba22f6151d31b526b823fb6892118bfcbce33
unknown <kacper2116124@gmail.com>
2026-08-16 16:11:48 +0200

    Dodano zbroje sura

--- src/data/itemsDB.js
+++ src/data/itemsDB.js
@@ -1,8 +1,10 @@
 import swords from './items/swords.json'
 import warriorArmours from './items/warrior_armours.json'
 import ninjaArmours from './items/ninja_armours.json'
+import suraArmours from './items/sura_armours.json'
 export const itemsDB = [
     ...swords,
     ...warriorArmours,
     ...ninjaArmours,
+    ...suraArmours,
 ]
