ff30130b188121fe1e5f9719914dc2d88928aed0
unknown <kacper2116124@gmail.com>
2026-08-16 16:17:59 +0200

    Dodano zbroje szaman

--- src/data/itemsDB.js
+++ src/data/itemsDB.js
@@ -2,9 +2,11 @@ import swords from './items/swords.json'
 import warriorArmours from './items/warrior_armours.json'
 import ninjaArmours from './items/ninja_armours.json'
 import suraArmours from './items/sura_armours.json'
+import shamanArmours from './items/shaman_armours.json'
 export const itemsDB = [
     ...swords,
     ...warriorArmours,
     ...ninjaArmours,
     ...suraArmours,
+    ...shamanArmours,
 ]
