26292b29577b56f3ce272577b4167a4d3768bd4a
unknown <kacper2116124@gmail.com>
2026-08-16 17:38:46 +0200

    Dodano sztylety i bonus na potwory

--- src/components/TooltipContent.jsx
+++ src/components/TooltipContent.jsx
@@ -29,7 +29,8 @@ const TooltipContent = ({ itemInstance }) => {
         movement_speed: { label: translate('stats.movement_speed') + ' ', unit: '%' },
         magic_resistance: { label: translate('stats.magic_resistance') + ': ', unit: '%' },
         max_hp: { label: translate('stats.max_hp') + ' +', unit: null },
-        strong_against_half_humans: { label: translate('stats.strong_against_half_humans') + ' +', unit: '%' }
+        strong_against_half_humans: { label: translate('stats.strong_against_half_humans') + ' +', unit: '%' },
+        strong_against_monsters: { label: translate('stats.strong_against_monsters') + ' +', unit: '%' }
     }
 
     const formatStatValue = (value) => {
--- src/data/itemsDB.js
+++ src/data/itemsDB.js
@@ -1,5 +1,6 @@
 import swords from './items/swords.json'
 import twoHanded from './items/two_handed.json'
+import daggers from './items/daggers.json'
 import warriorArmours from './items/warrior_armours.json'
 import ninjaArmours from './items/ninja_armours.json'
 import suraArmours from './items/sura_armours.json'
@@ -7,6 +8,7 @@ import shamanArmours from './items/shaman_armours.json'
 export const itemsDB = [
     ...swords,
     ...twoHanded,
+    ...daggers,
     ...warriorArmours,
     ...ninjaArmours,
     ...suraArmours,
