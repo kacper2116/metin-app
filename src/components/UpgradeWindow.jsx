7b5a217a67630acf8ae708b945a21b167acb4a97
unknown <kacper2116124@gmail.com>
2026-08-19 16:04:27 +0200

    Pozmieniano style przyciskow, zmieniono czcionke na pixel tahoma, dodano pasek z tytulem okna jak w metinie

--- src/components/UpgradeWindow.jsx
+++ src/components/UpgradeWindow.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import '../styles/UpgradeWindow.css'
 import Tooltip from './Tooltip'
 import TooltipContent from './TooltipContent'
+import Window from './Window'
+
 const UpgradeWindow = ({ itemToUpgrade }) => {
     console.log(itemToUpgrade)
     return (
+
         <div className='upgrade-window'>
+            <Window>
 
-            <div className='item-info'>
-                <div>
-                    <img className='item-thumbnail' src={itemToUpgrade.item.img}></img>
+                <div className='item-info'>
+                    <div>
+                        <img className='item-thumbnail' src={itemToUpgrade.item.img}></img>
+                    </div>
+                    <Tooltip isStatic={true}>
+                        <TooltipContent itemInstance={itemToUpgrade} />
+                    </Tooltip>
                 </div>
-                <Tooltip isStatic={true}>
-                    <TooltipContent itemInstance={itemToUpgrade} />
-                </Tooltip>
-            </div>
+            </Window>
+        </div >
 
-        </div>
     )
 }
 
---
+++ src/components/Window.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Window.css'
+import closeButton from '../assets/close-button.png'
+const Window = ({ children }) => {
+    return (
+        <div className='window'>
+            <div className='ui-container'>
+                <span className='window-title'>esssa</span>
+                <button className='close-button'>
+                    <img src={closeButton} />
+                </button>
+            </div>
+
+            <div className='window-content-container'>
+                <div className='window-content'> {children}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Window
