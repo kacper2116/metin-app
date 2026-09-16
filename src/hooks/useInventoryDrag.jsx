ae7c35a2b3b7f727652f2be3aee7b994ff7e7a5a
unknown <kacper2116124@gmail.com>
2026-09-16 18:34:09 +0200

    Naprawiono touchMove

--- src/hooks/useInventoryDrag.jsx
+++ src/hooks/useInventoryDrag.jsx
@@ -254,10 +254,9 @@ const useInventoryDrag = ({ items, setItems, activeTab, inventorySize, handleHov
             const moveX = Math.abs(e.clientX - startPos.current.x);
             const moveY = Math.abs(e.clientY - startPos.current.y);
 
-            if (moveX > 10 || moveY > 10) {
+            if (moveX > 50 || moveY > 50) {
                 clearTimeout(pressTimer.current);
                 pressTimer.current = null;
-
             }
 
             return;
@@ -266,6 +265,7 @@ const useInventoryDrag = ({ items, setItems, activeTab, inventorySize, handleHov
 
         const element = document.elementFromPoint(e.clientX, e.clientY);
         const slot = element?.closest('.slot');
+        console.log(slot)
 
         if (!slot) {
             clearSlotsPreview();
